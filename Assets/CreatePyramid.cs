6b751d162ddf05269ac3843305ac034b2acf77f0
dmedrzak <daniel.medrzak@gmail.com>
2017-05-17 09:04:54 +0200

    Piramida

---
+++ Assets/CreatePyramid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePyramid : MonoBehaviour
+{
+    public Material[] randomMaterials;
+    public GameObject PrefabKostki;
+    private GameObject lastGenerated;
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    if (k<=i && k<=9-i && k<=j && k<=9-j)
+                    {
+                        GameObject nowy = Instantiate(PrefabKostki);
+                        nowy.transform.position += new Vector3(20 + 3f * j, 3f * k, 3f * i);
+                        nowy.GetComponent<Renderer>().material =
+                            randomMaterials[Random.Range(0, randomMaterials.Length)];
+                        if (lastGenerated != null)
+                            nowy.GetComponent<SpringJoint>().connectedBody = lastGenerated.GetComponent<Rigidbody>();
+                        else
+                            Destroy(nowy.GetComponent<SpringJoint>());
+                        lastGenerated = nowy;
+                    }
+
+                }         
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject lufa;
+    public GameObject aroundObject;
+    public GameObject projectile;
+    public GameObject spawner;
+
+    public float bulletForce = 150f;
+    public float bulletSpeed = 20f;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject theBullet = Instantiate(projectile, spawner.transform.position, spawner.transform.rotation);
+            theBullet.GetComponent<Rigidbody>().velocity = spawner.transform.forward * bulletSpeed;
+
+
+        }
+    }
+}
+
+
+
+
